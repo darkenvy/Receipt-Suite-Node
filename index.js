e08940d5810e7faa08133e5384fff9476623b3a9
darkenvy <darkenvy6@gmail.com>
2016-11-21 14:12:55 -0800

    Sending photo off to node works. As long as data is in JSON Base64 format. {'base64':''}

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 var express = require('express')
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var tess = require('./tess-receipt');
 var fs = require('fs');
 var app = express();
 var sampleBase64Image = require('./images/sample-b64-image');
-// var rawSettings = {
-//   type: 'application/octet-stream',
-//   limit: '10mb'
-// }
+var rawSettings = {
+  limit: '10mb'
+}
 
 // app.use(bodyParser.raw(rawSettings));
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 // Problem with this method: Only one user can be processed at once.
 // Express will handle many users, but each file is written to tmp.jpg atm.
-app.get('/', function(req, res) {
-  fs.writeFile('images/tmp.jpg', new Buffer(sampleBase64Image, 'base64'), err => {
+// app.get('/', function(req, res) {
+//   fs.writeFile('images/tmp.jpg', new Buffer(sampleBase64Image, 'base64'), err => {
+//     tess('images/tmp.jpg').then(result => {
+//       console.log(result);
+//       res.send(JSON.stringify(result))
+//     });
+//   });
+// });
+
+app.post('/', bodyParser.json(rawSettings), function(req, res) {
+  fs.writeFile('images/tmp.jpg', new Buffer(req.body['base64'], 'base64'), () => {
     tess('images/tmp.jpg').then(result => {
       console.log(result);
       res.send(JSON.stringify(result))
     });
-  });
-});
-
-app.post('/', function(req, res) {
-  console.log(req.files);
-  // fs.writeFile('test.jpg', new Buffer(req.file, 'base64'), () => {
-    res.send('success')
-  // })
+  })
 });
 
 var server = app.listen(3000)
